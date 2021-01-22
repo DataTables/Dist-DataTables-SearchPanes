c23f088e0666bd7931f7baadb4678d924b73699c
Allan Jardine <allan.jardine@sprymedia.co.uk>
2021-01-22 14:20:25 +0000

    fix: Further fixes for issues with searchpanes and html entities
    Jira Issue DD-1830
    
    Sync to source repo @a9ec1aca35a2065cd882d38e5f8189d3a28e3a4c

--- js/dataTables.searchPanes.js
+++ js/dataTables.searchPanes.js
@@ -1278,12 +1278,10 @@
             // For each item selected in the pane, check if it is available in the cell
             for (var _i = 0, _a = this.selections; _i < _a.length; _i++) {
                 var colSelect = _a[_i];
-                if (typeof colSelect.filter === 'string') {
+                if (typeof colSelect.filter === 'string' && typeof filter === 'string') {
                     // The filter value will not have the &amp; in place but a &,
                     //  so we need to do a replace to make sure that they will match
-                    colSelect.filter = colSelect.filter.replace(/&amp;/g, '&');
-                    colSelect.filter = colSelect.filter.replace(/&lt;/g, '<');
-                    colSelect.filter = colSelect.filter.replace(/&gt;/g, '>');
+                    colSelect.filter = colSelect.filter.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>');
                 }
                 // if the filter is an array then is the column present in it
                 if (Array.isArray(filter)) {
