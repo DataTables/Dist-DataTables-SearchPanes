9c1963a1ec91d45e300639b7a8d008c6fd0e95b0
Allan Jardine <allan.jardine@sprymedia.co.uk>
2021-02-12 09:55:33 +0000

    fix: Fix SP issue with replacing & quot; and filtering on that
    Jira Issue DD-1830
    
    Sync to source repo @17422c68825190666d6750406fab27027a171fc6

--- js/dataTables.searchPanes.js
+++ js/dataTables.searchPanes.js
@@ -1292,7 +1292,7 @@
                 if (typeof colSelect.filter === 'string' && typeof filter === 'string') {
                     // The filter value will not have the &amp; in place but a &,
                     //  so we need to do a replace to make sure that they will match
-                    colSelect.filter = colSelect.filter.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+                    colSelect.filter = colSelect.filter.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
                 }
                 // if the filter is an array then is the column present in it
                 if (Array.isArray(filter)) {
