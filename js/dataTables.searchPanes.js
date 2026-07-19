01a88088e52c84103aedb6426be0f94db4a9d152
dtbuild <dtbuild@rigil>
2026-07-19 13:59:41 +0000

    ff1d8320e468eaccb95624a56f2f1300b9c41923 Examples: Buttons no longer needs extra files for the export buttons and column visibility.
    Sync to source repo @ff1d8320e468eaccb95624a56f2f1300b9c41923

--- js/dataTables.searchPanes.js
+++ js/dataTables.searchPanes.js
@@ -1,53 +1,6 @@
 /*! SearchPanes 2.3.5
  * © SpryMedia Ltd - datatables.net/license
  */
-
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		var jq = require('jquery');
-		var cjsRequires = function (root, $) {
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net')(root, $);
-			}
-		};
-
-		if (typeof window === 'undefined') {
-			module.exports = function (root, $) {
-				if ( ! root ) {
-					// CommonJS environments without a window global must pass a
-					// root. This will give an error otherwise
-					root = window;
-				}
-
-				if ( ! $ ) {
-					$ = jq( root );
-				}
-
-				cjsRequires( root, $ );
-				return factory( $, root, root.document );
-			};
-		}
-		else {
-			cjsRequires( window, jq );
-			module.exports = factory( jq, window, window.document );
-		}
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
-
 (function () {
     'use strict';
 
@@ -3571,7 +3524,3 @@ var DataTable = $.fn.dataTable;
     }
 
 })();
-
-
-return DataTable;
-}));
